0cda178283336665a36d81e412753f0182de61ab
Arslan <rslnkrmt2552@gmail.com>
2020-08-26 16:53:01 +0300

    Calculating metrics in parallel (146 ms)

--- corrRoutines.go
+++ corrRoutines.go
@@ -4,25 +4,14 @@ import (
 	"sync"
 )
 
-func sccWorker(n float64, server *CorrServer, abcdTable *[]dfRow, wg *sync.WaitGroup) {
+func sccWorker(n float64, table *CorrTable, abcdTable *[]dfRow, wg *sync.WaitGroup) {
 	defer wg.Done()
-	//var scc float64
-	var innerWg sync.WaitGroup
+	var scc float64
 	for i, row := range *abcdTable {
-		innerWg.Add(1)
-		go func(trow dfRow, idx int) {
-			defer innerWg.Done()
-			scc := sccCalc(trow.a, trow.b, trow.c, trow.d, n)
-			server.updateChannel <- UpdateRequest{
-				k: scc,
-				v: idx,
-			}
-		}(row, i)
-		//scc = sccCalc(row.a, row.b, row.c, row.d, n)
-		//table.updateTable(scc, i)
-	}
 
-	innerWg.Wait()
+		scc = sccCalc(row.a, row.b, row.c, row.d, n)
+		table.updateTable(scc, i)
+	}
 }
 
 func pearsonWorker(table *CorrTable, abcdTable *[]dfRow, wg *sync.WaitGroup) {
@@ -46,29 +35,5 @@ func otherCorrWorker(fn otherFn, table *CorrTable, abcdTable *[]dfRow, wg *sync.
 	for i, row := range *abcdTable {
 		corr = fn(row.a, row.b, row.c)
 		table.updateTable(corr, i)
-	}
-}
-
-type UpdateRequest struct {
-	k float64
-	v int
-}
-type CorrServer struct {
-	table         *CorrTable
-	updateChannel chan UpdateRequest
-}
-
-func NewCorrServer(tbl *CorrTable) *CorrServer {
-	server := &CorrServer{
-		table:         tbl,
-		updateChannel: make(chan UpdateRequest),
-	}
-
-	return server
-}
-
-func (s *CorrServer) loop(){
-	for update := range s.updateChannel{
-		s.table.updateTable(update.k, update.v)
 	}
 }
